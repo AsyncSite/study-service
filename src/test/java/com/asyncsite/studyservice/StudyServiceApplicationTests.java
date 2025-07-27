f807057213ac1bc10c0a33caf38f568ec37105f8
jo94kr <wls94kr@gmail.com>
2025-07-27 21:37:13 +0900

    feat: Implement Study management with JPA and add integration tests

--- src/test/java/com/asyncsite/studyservice/StudyServiceApplicationTests.java
+++ src/test/java/com/asyncsite/studyservice/StudyServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.asyncsite.studyservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StudyServiceApplicationTests {
 
 	@Test
