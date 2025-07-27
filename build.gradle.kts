41d4e5eab7d85d13ba8421d7e41ff92a918e9e2b
jo94kr <wls94kr@gmail.com>
2025-07-27 23:56:25 +0900

    feat: Add Logback configuration and update logging settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
 	// Core Platform Common
 	implementation("com.asyncsite.coreplatform:common:1.0.0-SNAPSHOT")
 
+	// logging
+	implementation("net.logstash.logback:logstash-logback-encoder:8.0")
+
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
